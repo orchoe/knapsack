cc8397e4c6b025b3f88d132bbd77bf4f6a16b4ea
orchoe <32832746+orchoe@users.noreply.github.com>
2018-11-26 13:53:08 +0800

    Update ScoreAndTime.java

--- src/main/java/com/codingcow/knapsack/pojo/ScoreAndTime.java
+++ src/main/java/com/codingcow/knapsack/pojo/ScoreAndTime.java
@@ -3,7 +3,6 @@ package com.codingcow.knapsack.pojo;
 /**
  * @Author: Austin Zhang
  * @Date: 2018-09-27 19:42
- * @Email: austin.zhang@dadaabc.com
  */
 public class ScoreAndTime implements Comparable<ScoreAndTime> {
 
